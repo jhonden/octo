163b56549865de3953a273fd926138bbb64dd686
chenlunjie <jhonden@163.com>
2026-03-14 12:56:06 +0800

    feat: initialize Spring Boot project with dependencies

---
+++ backend/src/main/java/com/skm/ServiceKnowledgeManagerApplication.java
@@ -0,0 +1,11 @@
+package com.skm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceKnowledgeManagerApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ServiceKnowledgeManagerApplication.class, args);
+  }
+}
